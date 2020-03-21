0b619fdbac497e794349cc8884b2c770aac179b6
mavspeter <mavspeter@gmail.com>
2020-03-21 12:58:03 +0300

    WIP

--- app/src/main/java/com/example/decorum/views/activities/IntroActivity.java
+++ app/src/main/java/com/example/decorum/views/activities/IntroActivity.java
@@ -1,4 +1,16 @@
 package com.example.decorum.views.activities;
 
-public class IntroActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.decorum.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- app/src/main/java/com/example/decorum/views/activities/SignUpActivity.java
+++ app/src/main/java/com/example/decorum/views/activities/SignUpActivity.java
@@ -1,4 +1,16 @@
 package com.example.decorum.views.activities;
 
-public class SignUpActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.decorum.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
