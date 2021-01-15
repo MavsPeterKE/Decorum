30cf46f7afb86663a8510041ccee79e8eb06cc4f
MavsPeterKE <ponyango@copiakenya.com>
2021-01-15 15:23:34 +0300

    Added Changes

--- app/src/main/java/com/example/decorum/views/activities/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/decorum/views/activities/ForgotPasswordActivity.java
@@ -1,16 +1,32 @@
 package com.example.decorum.views.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.decorum.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @BindView(R.id.btReset)
+    Button btReset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgot_password);
+        ButterKnife.bind(this);
+        btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ForgotPasswordActivity.this, "Password Reset Submitted Successfully", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ForgotPasswordActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/decorum/views/activities/SignUpActivity.java
+++ app/src/main/java/com/example/decorum/views/activities/SignUpActivity.java
@@ -1,16 +1,34 @@
 package com.example.decorum.views.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.eightbitlab.bottomnavigationbar.BottomNavigationBar;
 import com.example.decorum.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignUpActivity extends AppCompatActivity {
+    @BindView(R.id.btlogin)
+    Button btlogin;
+
+    @BindView(R.id.btstart)
+    RelativeLayout btStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        ButterKnife.bind(this);
+
+        btlogin.setOnClickListener(v -> startActivity(new Intent(SignUpActivity.this,LoginActivity.class)));
+
+        btStart.setOnClickListener(v -> startActivity(new Intent(SignUpActivity.this,HomeActivity.class)));
     }
 }
